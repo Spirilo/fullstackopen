ebbb637b1f10611850de83a39155e766ba584a25
Niko <rappe.niko@gmail.com>
2023-05-04 11:12:20 +0300

    Exercise 4.2

---
+++ part4/app.js
@@ -0,0 +1,23 @@
+const http = require('http')
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('Connection ok!')
+  })
+  .catch((error) => {
+    logger.error('ERROR connecting', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+
+module.exports = app
