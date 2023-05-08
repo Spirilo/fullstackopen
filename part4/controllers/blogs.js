e548fcb3c5dd14e2fce38752f1da4ef2143ccbec
Niko <rappe.niko@gmail.com>
2023-05-08 17:17:39 +0300

    Exercises 4.8-4.12 done

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,22 +1,25 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+blogsRouter.get('/', async (request, response) => {
+  let blogs = await Blog.find({})
+  response.json(blogs)
 })
   
-blogsRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body)
+blogsRouter.post('/', async (request, response) => {
+  const body = request.body
+  if (body.title === undefined || body.url === undefined) {
+    return response.status(400).end()
+  } 
+  let blog = new Blog({
+    author: body.author,
+    title: body.title,
+    likes: body.likes || 0,
+    url: body.url
+  })
   
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
 })
 
 module.exports = blogsRouter
---
+++ part4/tests/blogsApi.test.js
@@ -0,0 +1,105 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422a851b54a676234d17f7',
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 7,
+      __v: 0
+    }
+]
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    let blogObject = new Blog(initialBlogs[0])
+    await blogObject.save()
+    blogObject = new Blog(initialBlogs[1])
+    await blogObject.save()
+})
+
+test('returns right amount of blogs as JSON', async () => {
+    const response = await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    expect(response.body).toHaveLength(2)
+})
+test('identification is named id', async () => {    
+    const response = await api.get('/api/blogs')
+    const ids = response.body.map(b => b.id)
+
+    expect(ids).toBeDefined()
+})
+test('post works and blog is saved in right format', async () => {
+    const newBlog = {
+      title: 'Type wars',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+      likes: 2,
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const allNotes = await api.get('/api/blogs')
+    expect(allNotes.body).toHaveLength(initialBlogs.length + 1)
+})
+test('if likes is not defined, set it to 0', async () => {
+    const newBlog = {
+      title: 'Type wars',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html'
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+
+    const blog = await Blog.findOne({title: 'Type wars'})
+    expect(blog.likes).toBe(0)
+})
+test('POST to /api/blogs return status 400 if no title', async () => {
+    const newBlog = {
+        author: 'Robert C. Martin',
+        url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html'
+      }
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+    
+})
+test('POST to /api/blogs return status 400 if no url', async () => {
+    const newBlog = {
+        title: 'Type wars',
+        author: 'Robert C. Martin',
+      }
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+    
+})
+
+afterAll(async () => {
+    await mongoose.connection.close()
+})
