96ba7896884016421f2d42d0f98bd9e7905f1d34
Niko <rappe.niko@gmail.com>
2023-07-14 14:08:23 +0300

    Exercise 8.1

--- part8/index.js
+++ part8/index.js
@@ -98,14 +98,30 @@ let books = [
 */
 
 const typeDefs = `
+  type Author {
+    name: String!
+    born: Int
+    id: ID!
+  }
+
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: ID!
+    genres: [String!]
+  }
+
   type Query {
-    dummy: Int
+    bookCount: Int!
+    authorCount: Int!
   }
 `
 
 const resolvers = {
   Query: {
-    dummy: () => 0
+    bookCount: () => books.length,
+    authorCount: () => authors.length
   }
 }
 
