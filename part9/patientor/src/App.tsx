58b4c3621c526dce0bcd2a1c4f1ae24fac7d0cdc
Niko <rappe.niko@gmail.com>
2023-08-07 17:58:19 +0300

    Exercise 9.21

--- part9/patientor/src/App.tsx
+++ part9/patientor/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Routes, useMatch } from "react-router-dom";
 import { Button, Divider, Container, Typography } from '@mui/material';
 
 import { apiBaseUrl } from "./constants";
@@ -8,9 +8,15 @@ import { Patient } from "./types";
 
 import patientService from "./services/patients";
 import PatientListPage from "./components/PatientListPage";
+import PatientInfo from "./components/PatientInfo";
 
 const App = () => {
   const [patients, setPatients] = useState<Patient[]>([]);
+
+  const match = useMatch('/patients/:id');
+  const patient = match
+    ? patients.find(p => p.id === match.params.id)
+    : undefined
 
   useEffect(() => {
     void axios.get<void>(`${apiBaseUrl}/ping`);
@@ -24,7 +30,6 @@ const App = () => {
   
   return (
     <div className="App">
-      <Router>
         <Container>
           <Typography variant="h3" style={{ marginBottom: "0.5em" }}>
             Patientor
@@ -35,9 +40,9 @@ const App = () => {
           <Divider hidden />
           <Routes>
             <Route path="/" element={<PatientListPage patients={patients} setPatients={setPatients} />} />
+            <Route path="/patients/:id" element={<PatientInfo />} />
           </Routes>
         </Container>
-      </Router>
     </div>
   );
 };
---
+++ part9/patientor/src/components/PatientInfo/index.tsx
@@ -0,0 +1,33 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+import { Female, Male } from '@mui/icons-material';
+
+import { Patient } from "../../types";
+import patientService from '../../services/patients';
+
+const PatientInfo = () => {
+  const id = useParams().id;
+  const [patient, setPatient] = useState<Patient>()
+  
+  useEffect(() => {
+    const fetchPatient = async () => {
+      const patient = await patientService.getById(id);
+      setPatient(patient);
+    };
+    void fetchPatient();
+  })
+
+  return (
+    <div>
+      <h2>{patient?.name} 
+        {patient?.gender === 'male' && <Male />}
+        {patient?.gender === 'female' && <Female />}
+      </h2>
+      <p>ssn: {patient?.ssn}</p>
+      <p>occupation: {patient?.occupation}</p>
+    </div>
+  )
+};
+
+export default PatientInfo;
